b9ec0fda2980c9052c19cd38938355b302be9239
naotama (Naoki KOBAYASHI) <naoki-kobayashi@cybozu.co.jp>
2023-10-30 14:20:34 +0900

    5分ごとに実行する関数登録とdeploy環境

--- lib/cron-github-actions-workflow-from-lambda-stack.ts
+++ lib/cron-github-actions-workflow-from-lambda-stack.ts
@@ -1,16 +1,28 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import * as events from "aws-cdk-lib/aws-events";
+import * as targets from 'aws-cdk-lib/aws-events-targets';
 
 export class CronGithubActionsWorkflowFromLambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const WorkflowDispatch = new NodejsFunction(this, "WorkflowDispatch", {
+      runtime: Runtime.NODEJS_18_X,
+      entry: "lib/trigger-ga-workflow-dispatch.ts",
+      timeout: cdk.Duration.seconds(60),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CronGithubActionsWorkflowFromLambdaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new events.Rule(this, "WorkflowDispatchRule", {
+      // 5分ごとに実行
+      schedule: events.Schedule.expression("cron(0/5 * * * ? *)"),
+      targets: [new targets.LambdaFunction(WorkflowDispatch,
+        {
+          retryAttempts: 0
+        }
+      )],
+    });
   }
 }
