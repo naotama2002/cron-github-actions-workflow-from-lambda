86ac822a72cf266af8e818dc04364ce529b60494
naotama (Naoki KOBAYASHI) <naoki-kobayashi@cybozu.co.jp>
2023-10-30 16:43:04 +0900

    Secrets Manager を読むための権限付与

--- lib/cron-github-actions-workflow-from-lambda-stack.ts
+++ lib/cron-github-actions-workflow-from-lambda-stack.ts
@@ -1,28 +1,37 @@
-import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { Runtime } from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Secret } from "aws-cdk-lib/aws-secretsmanager";
 import * as events from "aws-cdk-lib/aws-events";
 import * as targets from 'aws-cdk-lib/aws-events-targets';
+import * as cdk from "aws-cdk-lib";
+import * as dotenv from "dotenv";
 
 export class CronGithubActionsWorkflowFromLambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const WorkflowDispatch = new NodejsFunction(this, "WorkflowDispatch", {
-      runtime: Runtime.NODEJS_18_X,
-      entry: "lib/trigger-ga-workflow-dispatch.ts",
-      timeout: cdk.Duration.seconds(60),
-    });
+    const stringSecretArn = process.env.AWS_SECRETS_MANAGER_APN;
+    if (stringSecretArn !== undefined) {
+      const WorkflowDispatch = new NodejsFunction(this, "WorkflowDispatch", {
+        runtime: Runtime.NODEJS_18_X,
+        entry: "lib/trigger-ga-workflow-dispatch.ts",
+        timeout: cdk.Duration.seconds(60),
+      });
 
-    new events.Rule(this, "WorkflowDispatchRule", {
-      // 5分ごとに実行
-      schedule: events.Schedule.expression("cron(0/5 * * * ? *)"),
-      targets: [new targets.LambdaFunction(WorkflowDispatch,
-        {
-          retryAttempts: 0
-        }
-      )],
-    });
+    // AWS Secrets Manager への権限付与
+    const smResource = Secret.fromSecretCompleteArn(this, "SecretsManager", stringSecretArn);
+      smResource.grantRead(WorkflowDispatch);
+
+      new events.Rule(this, "WorkflowDispatchRule", {
+        // 5分ごとに実行
+        schedule: events.Schedule.expression("cron(0/5 * * * ? *)"),
+        targets: [new targets.LambdaFunction(WorkflowDispatch,
+          {
+            retryAttempts: 0
+          }
+        )],
+      });
+    }
   }
 }
