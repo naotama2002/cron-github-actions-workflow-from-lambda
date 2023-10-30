72c63467b8b3c66b6a636a2dacdbbb6714dcf2ff
naotama (Naoki KOBAYASHI) <naoki-kobayashi@cybozu.co.jp>
2023-10-30 17:54:51 +0900

    いろいろ動くように修正したよ

--- lib/cron-github-actions-workflow-from-lambda-stack.ts
+++ lib/cron-github-actions-workflow-from-lambda-stack.ts
@@ -11,20 +11,30 @@ export class CronGithubActionsWorkflowFromLambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // .env ファイル読み込み
+    dotenv.config();
+
+    // Secrets Manager の APN
     const stringSecretArn = process.env.AWS_SECRETS_MANAGER_APN;
+
+    // Secrets Manager が設定されている場合のみ Lambda 登録
+    // ここで確認するのが良いのか？という議論は、実業務に入れる時に考える
+    // なんでここで確認してるの？に興味がある人は、Enginnering Producitivity Meetup #1 in Tokyo でお話ししましょ (https://cybozu.connpass.com/event/298452/)
     if (stringSecretArn !== undefined) {
+      // Secrets Manager APN
       const WorkflowDispatch = new NodejsFunction(this, "WorkflowDispatch", {
         runtime: Runtime.NODEJS_18_X,
-        entry: "lib/trigger-ga-workflow-dispatch.ts",
+        entry: "lib/lambda-handler.ts",
         timeout: cdk.Duration.seconds(60),
       });
 
-    // AWS Secrets Manager への権限付与
-    const smResource = Secret.fromSecretCompleteArn(this, "SecretsManager", stringSecretArn);
+      // AWS Secrets Manager への権限付与
+      //   Secrets Manager の該当 APN の Read権限
+      const smResource = Secret.fromSecretCompleteArn(this, "SecretsManager", stringSecretArn);
       smResource.grantRead(WorkflowDispatch);
 
+      // 5分ごとに実行
       new events.Rule(this, "WorkflowDispatchRule", {
-        // 5分ごとに実行
         schedule: events.Schedule.expression("cron(0/5 * * * ? *)"),
         targets: [new targets.LambdaFunction(WorkflowDispatch,
           {
@@ -32,6 +42,8 @@ export class CronGithubActionsWorkflowFromLambdaStack extends cdk.Stack {
           }
         )],
       });
+    } else {
+      throw new Error("Secrets Manager の APN が .env ファイルから読み込めませんでした。.env.sample を .env にコピーして Secrets Manager の APN を設定してください。");
     }
   }
 }
